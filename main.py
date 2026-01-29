84cf918ef59ea40bb75f512f4378073e0a7fa870
itsmconsultant <sm.consultant.it@gmail.com>
2026-01-29 20:21:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from upload_data import show_upload_dashboard
 from process_data import show_run_procedure
 from report_rekonsiliasi_transaksi_deposit_dan_settlement import show_report_deposit_settlement
 from report_rekonsiliasi_transaksi_disbursement_dan_saldo_durian import show_report_disbursement_durian
-from detail_reversal import show_report_detail_reversal
+from report_detail_reversal import show_report_detail_reversal
 
 # 1. SET WIDE MODE DEFAULT
 st.set_page_config(
