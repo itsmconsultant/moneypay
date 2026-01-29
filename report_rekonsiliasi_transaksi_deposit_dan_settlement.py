d234ec0964ed997993e8151fde82b696f1586b76
itsmconsultant <sm.consultant.it@gmail.com>
2026-01-29 20:04:55 +0700

    Update report_rekonsiliasi_transaksi_deposit_dan_settlement.py

--- report_rekonsiliasi_transaksi_deposit_dan_settlement.py
+++ report_rekonsiliasi_transaksi_deposit_dan_settlement.py
@@ -26,7 +26,7 @@ def show_report_deposit_settlement(conn):
                 # 3. Menggunakan API Select Supabase (Tanpa SQLAlchemy)
                 # .table() merujuk ke nama tabel, .select("*") mengambil semua kolom
                 response = conn.client.schema("project1").table("summary_deposit") \
-                    .select("*") \
+                    .select("merchant, tanggal_proses, keterangan, jumlah_transaksi, jumlah_transaksi_sesuai_rate, penambahan_rupiah, pengurangan_rupiah, rekonsiliasi_jumlah_transaksi, rekonsiliasi_rupiah, rekonsiliasi_tambah_kurang, saldo_rekonsiliasi_rupiah") \
                     .eq("tanggal_proses", tanggal_str) \
                     .order("urutan", desc=False) \
                     .execute()
