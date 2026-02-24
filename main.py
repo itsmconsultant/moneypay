aac08d837b935f5c763b32587035a334e7e524ce
itsmconsultant <sm.consultant.it@gmail.com>
2026-02-24 09:24:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.set_page_config(
 )
 
 # 2. KONEKSI KE SUPABASE
-conn = st.connection("supabase", type=SupabaseConnection, ttl=None)
+conn = st.connection("supabase", type=SupabaseConnection, ttl=0)
 
 # 3. INISIALISASI SESSION STATE DASAR
 if "authenticated" not in st.session_state:
